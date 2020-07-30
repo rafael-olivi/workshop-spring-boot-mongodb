a997d3bdadad45d7c1d5ab74a764314ca1e04cc8
Timba <rbolivi@hotmail.com>
2020-07-30 00:15:53 -0300

    First Commit - Create Project

---
+++ src/test/java/com/timba/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timba.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
