ef8a9bfed544d05467c62beceff788b61064881e
Timba <rbolivi@hotmail.com>
2020-07-31 00:40:44 -0300

    Metodo de Insert User

--- src/main/java/com/timba/workshopmongo/services/UserService.java
+++ src/main/java/com/timba/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.timba.workshopmongo.domain.User;
+import com.timba.workshopmongo.dto.UserDTO;
 import com.timba.workshopmongo.repository.UserRepository;
 import com.timba.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,14 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public User insert(User entity) {
+		return repo.insert(entity);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
+	
 }
