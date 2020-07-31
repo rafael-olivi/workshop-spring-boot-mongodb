24aac19308e29f975fbd99a2300e46a7c3baea42
Timba <rbolivi@hotmail.com>
2020-07-31 00:57:12 -0300

    Metodo Update User

--- src/main/java/com/timba/workshopmongo/services/UserService.java
+++ src/main/java/com/timba/workshopmongo/services/UserService.java
@@ -35,6 +35,17 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User entity) {
+		User newEntity = findById(entity.getId());
+		updateData(newEntity, entity);
+		return repo.save(newEntity);
+	}
+	
+	private void updateData(User newEntity, User entity) {
+		newEntity.setName(entity.getName());
+		newEntity.setEmail(entity.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
