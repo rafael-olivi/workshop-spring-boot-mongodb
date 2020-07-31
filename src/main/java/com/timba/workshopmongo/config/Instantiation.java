2d4a7e211342986068803bee70be1695a1994083
Timba <rbolivi@hotmail.com>
2020-07-31 01:54:36 -0300

    Create Post Entity

--- src/main/java/com/timba/workshopmongo/config/Instantiation.java
+++ src/main/java/com/timba/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.timba.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.timba.workshopmongo.domain.Post;
 import com.timba.workshopmongo.domain.User;
+import com.timba.workshopmongo.repository.PostRepository;
 import com.timba.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,16 +19,27 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	UserRepository userRepository;
 	
+	@Autowired
+	PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partiu", "Viajar São Paulo", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia", "Acordei Feliz!", maria);
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
---
+++ src/main/java/com/timba/workshopmongo/domain/Post.java
@@ -0,0 +1,26 @@
+package com.timba.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document
+public class Post implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@org.springframework.data.annotation.Id
+	private String Id;
+	private Date date;
+	private String title;
+	private String body;
+	private User author;
+	
+}
