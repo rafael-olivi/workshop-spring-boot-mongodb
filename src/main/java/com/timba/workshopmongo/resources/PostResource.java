98410965aa5449b671d685deaa53a5a4d03b2d11
Timba <rbolivi@hotmail.com>
2020-08-02 23:30:41 -0300

    Consulta simples com query methods

--- src/main/java/com/timba/workshopmongo/resources/PostResource.java
+++ src/main/java/com/timba/workshopmongo/resources/PostResource.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.timba.workshopmongo.domain.Post;
+import com.timba.workshopmongo.resources.util.URL;
 import com.timba.workshopmongo.services.PostService;
 
 @RestController
@@ -29,5 +31,12 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id){		
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text){		
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
